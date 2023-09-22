a5fdc43ba0a208f2fbbb88fe56e7fc4132d2422e
Ruan pelantir <106276664+ruanpelantir@users.noreply.github.com>
2023-09-22 08:17:17 -0300

    aula 22/09

--- aluramidi-curso-arquivos-iniciais/maim.js
+++ aluramidi-curso-arquivos-iniciais/maim.js
@@ -1,4 +1,37 @@
-alert('seu computador foi hackeado saia agora, risco de explosao');
+alert('vc foi hackeado');
 
-document.querySelector('.tecla_pom');
-document.querySelector('#son_tecla_pom').play();
+ function tocarSomPom(){document.querySelector('.tecla_pom');
+}
+document.querySelector('tecla_pom').oncli = tocarSomPom;
+
+function tocarSomClap(){document.querySelector('.tecla_Clap');
+}
+document.querySelector('tecla_Clap').oncli = tocarSomClap;
+
+function tocarSomTim(){document.querySelector('.tecla_Tim');
+}
+document.querySelector('tecla_Tim').oncli = tocarSomTim;
+
+function tocarSomPuf(){document.querySelector('.tecla_Puf');
+}
+document.querySelector('tecla_PUf').oncli = tocarSomPuf;
+
+function tocarSomSplash(){document.querySelector('.tecla_Splash');
+}
+document.querySelector('tecla_Splash').oncli = tocarSomSplash;
+
+function tocarSomToim(){document.querySelector('.tecla_Toim');
+}
+document.querySelector('tecla_Toim').oncli = tocarSomToim;
+
+function tocarSomPsh(){document.querySelector('.tecla_Psh');
+}
+document.querySelector('tecla_Psh').oncli = tocarSomPsh;
+
+function tocarSomTic(){document.querySelector('.tecla_Tic');
+}
+document.querySelector('tecla_Tic').oncli = tocarSomTic;
+
+function tocarSomTom(){document.querySelector('.tecla_Tom');
+}
+document.querySelector('tecla_Tom').oncli = tocarSomTom;
